a4e42e21e99c0f09f61db6e1a3ef6cc7ad82d0d2
Evgeniy Sushchevich <rupychman@mail.ru>
2023-07-31 21:48:33 +0300

    fix: setting checked

--- src/modules/Settings/Settings.tsx
+++ src/modules/Settings/Settings.tsx
@@ -1,10 +1,23 @@
-import { Response } from "@/__generated__/graphql";
+import { Response, Settings } from "@/__generated__/graphql";
 import { SET_ACTIVATION_BY_LINK } from "@/graphql/mutations";
-import { useMutation } from "@apollo/client";
+import { GET_SETTINGS } from "@/graphql/query";
+import { useMutation, useQuery } from "@apollo/client";
 import { Form, Switch, notification } from "antd";
+import { QueryOptions } from 'apollo-client';
 
 function SettingsModule() {
     const [form] = Form.useForm();
+    const getSettings = useQuery<{ GetSettings: Settings }>(
+        GET_SETTINGS,
+        {
+            onError: error => {
+                notification.error({
+                    message: 'Ошибка',
+                    description: error?.message,
+                })
+            },
+        }
+    );
     const [setActivationByLink, { loading }] = useMutation<{ ConfirmActivation: Response }, { activationByLink: boolean }>(
         SET_ACTIVATION_BY_LINK,
         {
@@ -19,14 +32,17 @@ function SettingsModule() {
                     message: 'Успешно!',
                     description: 'Активация по ссылке была изменена.',
                 })
-            }
+            },
+            refetchQueries: [{
+                query: GET_SETTINGS,
+            } as QueryOptions<object>]
         }
     );
     return (
         <Form form={form}>
             <Form.Item name='active' label={'Активация по коду'}>
                 <Switch
-                    defaultChecked={true}
+                    checked={getSettings.data?.GetSettings.activationByLink}
                     loading={loading}
                     onChange={(value: boolean) =>
                         setActivationByLink({
